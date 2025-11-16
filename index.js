5235f342b03842fb72d868b314fec6ffe462a474
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2025-11-15 20:18:35 -0500

    fix tests

--- index.js
+++ index.js
@@ -1196,8 +1196,8 @@ class PlayerState {
         // Input only (not modified)
         this.attributes = bot.entity.attributes
         // both rotational values in degrees (notchian format). they should be float32 to replicate what the server should receive
-        this.yawDegrees = new JavaFloat(bot.entity.yawDegrees)
-        this.pitchDegrees = new JavaFloat(bot.entity.pitchDegrees)
+        this.yawDegrees = bot.entity.yawDegrees ? new JavaFloat(bot.entity.yawDegrees) : new JavaFloat((Math.PI - bot.entity.yaw) * RAD_TO_DEG)
+        this.pitchDegrees = bot.entity.pitchDegrees ? new JavaFloat(bot.entity.pitchDegrees) : new JavaFloat(-bot.entity.pitch * RAD_TO_DEG)
         this.control = control
 
         // effects
